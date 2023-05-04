9b23acb66aa10c699cb62ce8c49ef4d2efdb38ae
Everton <imevertonrch@gmail.com>
2023-05-04 02:02:37 -0300

    Implementando paginação e ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,19 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosMedicoRequisicao;
+import med.voll.api.medico.dto.DadosMedicoRequisicao;
 import med.voll.api.medico.Medico;
+import med.voll.api.medico.dto.MedicoDataList;
 import med.voll.api.medico.repository.MedicoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -29,5 +33,12 @@ public class MedicoController {
 
         medicoRepository.save(medico);
         return ResponseEntity.ok(medicoRequisicao);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<MedicoDataList>> listMedicos(@PageableDefault(size = 1, sort = {"nome"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        System.out.println(pageable);
+        Page<Medico> medicos = medicoRepository.findAll(pageable);
+        return ResponseEntity.ok(medicos.map(MedicoDataList::new));
     }
 }
