fbbb8ae571a3fecbabe6058a8bd6c80bad62d62e
Everton <imevertonrch@gmail.com>
2023-05-05 18:04:55 -0300

    Exceção personalizada para token não enviado no header

---
+++ src/main/java/med/voll/api/security/exception/TokenNuloException.java
@@ -0,0 +1,8 @@
+package med.voll.api.security.exception;
+
+public class TokenNuloException extends RuntimeException{
+
+    public TokenNuloException(String message) {
+        super(message);
+    }
+}
