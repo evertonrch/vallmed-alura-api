4f9fea0d53e54825f8d91a462ad83d98f554c86a
Everton <imevertonrch@gmail.com>
2023-05-05 20:56:14 -0300

    Recuperando usuário através do token e forçando autenticação

--- src/main/java/med/voll/api/security/SecurityFilter.java
+++ src/main/java/med/voll/api/security/SecurityFilter.java
@@ -5,6 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import med.voll.api.repository.UsuarioRepository;
+import org.hibernate.event.spi.SaveOrUpdateEvent;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,21 +22,26 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
+    private final UsuarioRepository usuarioRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String tokenJWT = headerAuth(request);
-        var subject = tokenService.getSubject(tokenJWT);
-        System.out.println(subject);
+        if (tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            var usuario = usuarioRepository.findByLogin(subject);
 
-        filterChain.doFilter(request,response);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+
     }
 
     private String headerAuth(HttpServletRequest request) {
         var token = request.getHeader("Authorization");
-        if(token == null)
-            throw new TokenNuloException("Token não enviado");
-
-        return token.replaceAll("Bearer ", "");
+        if (token != null)
+            return token.replaceAll("Bearer ", "");
+        return null;
     }
 }
