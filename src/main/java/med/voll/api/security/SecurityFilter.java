92fd5d8fab371b626cf83316b306ab05bc09b6b0
Everton <imevertonrch@gmail.com>
2023-05-05 19:03:12 -0300

    Validando token recebido

--- src/main/java/med/voll/api/security/SecurityFilter.java
+++ src/main/java/med/voll/api/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,14 +12,17 @@ import med.voll.api.security.exception.TokenNuloException;
 
 import java.io.IOException;
 
+@AllArgsConstructor
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String tokenJWT = headerAuth(request);
-
-        System.out.println(tokenJWT);
+        var subject = tokenService.getSubject(tokenJWT);
+        System.out.println(subject);
 
         filterChain.doFilter(request,response);
     }
